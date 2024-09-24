8ff3d0f05f7646c5e7dd480221866f6cb10d67ab
SATHVIK_KOTA <lalitheshsathvik777@gmail.com>
2024-09-24 16:31:25 +0530

    Added Header Component

---
+++ frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order Your Food</h2>
+        <p>"Explore our diverse menu filled with delectable dishes crafted from the freshest ingredients, sure to tantalize your taste buds."</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
