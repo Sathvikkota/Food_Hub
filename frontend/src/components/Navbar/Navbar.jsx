1bc33f9041beef25d00c3d43da05df780ce2b207
SATHVIK_KOTA <lalitheshsathvik777@gmail.com>
2024-09-24 16:27:11 +0530

    Updated the Navbar Component

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -1,30 +1,34 @@
-import React, { useState } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
-import { assets } from '../../assets/frontend_assets/assets'
+import {assets} from '../../assets/assets' 
+import { Link } from 'react-router-dom';
+import { StoreContext } from '../../context/StoreContext';
 
-const Navbar = () => {
-    const [menu,setMenu]=useState("home");
-  return (
-    <div>
-        <div className="navbar">
-            <img src={assets.logo} alt="" className="logo" />
+const Navbar = ({setShowLogin}) =>{
+
+    const[menu,setMenu] = useState("Home");
+
+    const{getTotalCartAmount}= useContext(StoreContext)
+
+    return(
+        <div className='navbar'>
+            <Link to='/'><img src={assets.logo} alt="" className="logo" /></Link>
             <ul className="navbar-menu">
-                <li onClick={()=>setMenu("home")} className={menu==="home"?"active":""}>Home</li>
-                <li onClick={()=>setMenu("menu")} className={menu==="menu"?"active":""}>Menu</li>
-                <li onClick={()=>setMenu("app")} className={menu==="app"?"active":""}>App</li>
-                <li onClick={()=>setMenu("contact-us")} className={menu==="contact-us"?"active":""}>Contact Us</li>
+                <Link to='/' onClick={()=> setMenu("Home")} className={menu==="Home"?"active":""}>Home</Link>
+                <a href='#explore-menu' onClick={()=> setMenu("Menu")} className={menu==="Menu"?"active":""}>Menu</a>
+                <a href='#app-download' onClick={()=> setMenu("Mobile-App")} className={menu==="Mobile-App"?"active":""}>Mobile-App</a>
+                <a href='#footer' onClick={()=> setMenu("Contact-Us")} className={menu==="Contact-Us"?"active":""}>Contact-Us</a>
             </ul>
             <div className="navbar-right">
                 <img src={assets.search_icon} alt="" />
                 <div className="navbar-search-icon">
-                    <img src={assets.basket_icon} alt="" />
-                    <div className="dot"></div>
+                    <Link to='/cart'><img src={assets.basket_icon} alt="" /></Link>
+                    <div className={getTotalCartAmount()===0?"":"dot"}></div>
                 </div>
-                <button>Sign In</button>
+                <button onClick={()=>setShowLogin(true)}>Sign In</button>
             </div>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
